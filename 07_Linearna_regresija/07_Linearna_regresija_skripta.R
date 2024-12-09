b748c6e6d891babcca80e9ef8d8d3eb54d752b7b
lucijakanjer <40788855+lucijakanjer@users.noreply.github.com>
2024-12-09 17:00:09 +0100

    Add files via upload

---
+++ 07_Linearna_regresija/07_Linearna_regresija_skripta.R
@@ -0,0 +1,86 @@
+# Analize podataka u biološkim istraživanjima - praktikum 2024
+# Lucija Kanjer lucija.kanjer@biol.pmf.hr
+
+###############################################################################
+
+# 7. Linearna regresija
+
+# Uključene su sve korištene naredbe, ALI NEKE SU NEPOTPUNE ILI SADRŽE NAMJERNE POGREŠKE:
+# Dovršite naredbe gdje je potrebno i dodajte komentare za sebe (koristite # za komentiranje)
+#
+# IZVRŠAVANJE NAREDBI: ctrl + Enter 
+#
+###############################################################################
+
+# Instalacija i učitavanje potrebnih paketa
+install.packages("car")
+install.packages("broom")
+
+# Učitajte potrebne pakete
+library(car) # 'car' za vizualizaciju
+library(broom) # 'broom' za analizu modela
+library(ggplot2) # grafovi
+
+# Postavljanje radnog direktorija
+getwd()
+setwd("C:/Users/Hrvoje/Documents/APUBI/07_Regresije/")
+
+# Učitajte podatke iz CSV datoteke
+# Podaci sadrže informacije o gustoći drveća, kabina i krupnom drvenom otpadu
+jezera <- read.csv("christensen1996.csv")
+
+# Pogledajte prvih nekoliko redaka podataka kako biste razumjeli strukturu
+head(jezera)
+
+# Varijable:
+# jezero - 16 jezera u Sjevernoj Americi
+# obalno_drvece - gustoća obalnog drveća po kilometru (km−1)
+# drvni_otpad - površina drvnog otpada u m2 po kilometru (m2 km−1)
+# kolibe - gustoća ljudskih koliba po kilometru (no. km−1)
+
+
+# Vizualizacija podataka
+
+# Scatter plot odnosa drvnog otpada i gustoće obalnog drveća
+ggplot(jezera, aes(x = obalno_drvece, y = drvni_otpad)) +            
+  geom_point() +                                      
+  geom_smooth(method = "lm") +
+  theme_minimal()
+
+# Što prikazuje linija na scatterplotu?
+
+### Linearna regresija
+# nezavisna varijabla: obalno_drvece
+# zavisna varijabla: drvni_otpad
+
+# Linearni Model: Utjecaj gustoće obalnog drveća na količinu drvnog otpada
+model_drvece <- lm(drvni_otpad ~ obalno_drvece, data = jezera)
+
+# Ispis sažetka modela za interpretaciju koeficijenata
+summary(model_drvece)
+
+# Grafička dijagnostika modela
+par(mfrow = c(2, 2))
+plot(model_drvece)
+
+# Pogledajte rezidualne grafikone kako biste provjerili pretpostavke linearnog modela!
+
+# Dodatno - ANOVA i predict
+
+anova(model_drvece)
+# Koje su vrijednosti sume kvadrata za model, a koji za pogršku?
+# Koliko ima stupnjeva slobode?
+
+
+predict(model_drvece, data.frame(obalno_drvece = c(1500)), interval = "confidence")
+
+## Zadatak: Utjecaj koliba na količinu drvnog otpada
+
+# Napravite scatterplot odnosa drvnog otpada i koliba
+# Kreirajte novi model "model_kolibe" sa zavisnom varijabla: drvni_otpad i nezavisnom varijabla: kolibe
+# Napravite grafičku dijagnostiku modela: kakva je normalnost reziduala? Jesu li varijance reziduala konstantne?
+# Ispišite summary modela kolibe.
+
+
+
+
